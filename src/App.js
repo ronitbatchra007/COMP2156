c026738ab0cd1eaf0f5307f8ed7b973e18c7ce94
ronitbatchra007 <ronit_batchra@georgebrown.ca>
2025-02-06 01:26:08 -0500

    including application source

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Student ID: 101536292</h1>
+      <h1>Student Name: Ronit Batchra</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developed By: Ronit Batchra
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
